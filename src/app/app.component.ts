3c84563dadd00034efec96faa194f3008821f13b
Ramgopal81 <ramgopalrai566@gmail.com>
2024-04-02 13:08:25 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Report';
+  applicantId: any;
+  viewUrl: string = '';
+  constructor(private router: Router) {}
+
+  reportDownload() {
+    this.viewUrl = `http://4.236.144.236:8080/borrowerAppPhase2/jasper/report/${this.applicantId}`;
+    console.log(this.viewUrl);
+    window.open(this.viewUrl, '_blank');
+  }
 }
